ca6cc12d3c3e98a860cf11ba04dd136e8b6a98fd
Maxim Khrypunov <khrypunovmax@gmail>
2024-07-30 14:24:28 +0300

    HW-41

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -3,16 +3,24 @@ package telran.net;
 import java.net.*;
 import java.io.*;
 
-public class TcpClientServerSession {
+public class TcpClientServerSession extends Thread{
 	Socket socket;
 	Protocol protocol;
- 
-	public TcpClientServerSession(Socket socket, Protocol protocol) {
+	TcpServer tcpServer;
+
+	public TcpClientServerSession(Socket socket, Protocol protocol, TcpServer tcpServer) {
 		this.socket = socket;
 		this.protocol = protocol;
+		this.tcpServer = tcpServer;
+		try {
+            socket.setSoTimeout(60000); 
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
 	}
 	public void run() {
-		try (BufferedReader receiver =
+		try 	(Socket s = socket; // само закроет сокет!
+				BufferedReader receiver =
 				new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream sender = new PrintStream(socket.getOutputStream())){
 			String line = null;
@@ -20,10 +28,14 @@ public class TcpClientServerSession {
 				String responseStr = protocol.getResponseWithJSON(line);
 				sender.println(responseStr);
 			}
-			
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+		 } catch (SocketTimeoutException e) {
+	            if (!tcpServer.running) {
+	                System.out.println("Session closed via to server shutdown.");
+	            } else {
+	                System.out.println("Session closed via to idle timeout.");
+	            }
+	        } catch (IOException e) {
+	            System.out.println(e);
+	        } 
+	    }
 	}
-
-}
--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -3,24 +3,31 @@ import java.net.*;
 public class TcpServer {
 	Protocol protocol;
 	int port;
+	boolean running = true;
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
 	}
+	public void shutdown() {
+		running = false;
+	}
 	public void run() {
 		try(ServerSocket serverSocket = new ServerSocket(port)){
+			serverSocket.setSoTimeout(30000);
 			System.out.println("Server is listening on port " + port);
-			while(true) {
-				Socket socket = serverSocket.accept();
-				TcpClientServerSession session =
-						new TcpClientServerSession(socket, protocol);
-				session.run();
+			while(running) {
+				Socket socket;
+				try {
+					socket = serverSocket.accept();
+					TcpClientServerSession session =
+					new TcpClientServerSession(socket, protocol, this);
+				session.start();
+				} catch(SocketTimeoutException e) {
+					System.out.println("Server socket timed out.");
+				}
 			}
-				
-			
 		} catch(Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
 }
